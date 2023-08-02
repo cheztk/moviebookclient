ed86784703933b53300991b906ecb0aa4ee8b2d2
cheztk <ckengcon@gmail.com>
2023-08-02 09:48:48 +0700

    booking-list

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import {Tabs} from 'antd';
 import PageTitle from '../../components/PageTitle';
 import TheatresList from './TheatresList';
+import Bookings from './Bookings';
 
 function Profile() {
     return (
@@ -9,7 +10,7 @@ function Profile() {
 
       <Tabs defaultActiveKey="1">
         <Tabs.TabPane tab="Bookings" key="1">
-          Bookings
+          <Bookings/>
         </Tabs.TabPane>
         <Tabs.TabPane tab="Theatres" key="2">
           <TheatresList/>  
